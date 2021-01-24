8c34e37e5c6c0eba752dd78455159f063f0b8ca8
beshirD <beshirdek@gmail.com>
2021-01-24 22:25:31 +0300

    commit

--- sortColors.py
+++ sortColors.py
@@ -1,23 +1,11 @@
 def sortColors(nums):
-    left = 0
-    right = len(nums)-1
-    i = 0
-    while i <= right:
-        if nums[i] == 0:
-            nums[left] = nums[i]
-            i += 1
-            left += 1
-            print(nums)
-        elif nums[i] == 2:
-            temp = nums[i]
-            nums[i] = nums[right]
-            nums[right] = temp
-            right -= 1
-            i+=1
-            print(nums)
-        else:
-            i += 1
-            print(nums)
+    for i in range(len(nums)):
+        for j in range(i,len(nums)):
+            if nums[i] > nums[j]:
+                temp = nums[i]
+                nums[i] = nums[j]
+                nums[j] = temp
+                
     print(nums)
     
 sortColors([2,0,2,1,2,1,0])
